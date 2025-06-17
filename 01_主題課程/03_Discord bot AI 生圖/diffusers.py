accb8b262ec504be1b120f6c7fa7f61e78e71275
alifestone <95862568+alifestone@users.noreply.github.com>
2025-06-17 16:41:41 +0800

    Create diffusers.py

---
+++ 01_主題課程/03_Discord bot AI 生圖/diffusers.py
@@ -0,0 +1,17 @@
+import torch
+from diffusers import StableDiffusionPipeline
+
+# 初始化 Stable Diffusion
+model_id = "ckpt/anything-v4.5"
+pipe = StableDiffusionPipeline.from_pretrained(
+    model_id,
+    allow_pickle=False  # Add this line to prevent unsafe serialization
+)
+#diffusers reference: https://huggingface.co/docs/diffusers/main/en/quicktour
+#from_pretrained reference: https://huggingface.co/docs/diffusers/v0.32.2/en/api/models/overview#diffusers.ModelMixin.from_pretrained
+#pipeline reference: https://huggingface.co/docs/diffusers/main/en/api/pipelines/overview
+
+pipe = pipe.to("cuda")
+prompt = "a cut cat" #請填入你的prompt
+image = pipe(prompt).images[0]
+image.save("output.png")
