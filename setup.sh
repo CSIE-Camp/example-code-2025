e89530d7638fc5178b1856195df9c04cf5db2336
Takala <dylan91011028@gmail.com>
2025-06-24 13:05:02 +0800

    fix: reorder commands to clone repository before installing requirements

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ if (-not (Get-Command python -ErrorAction SilentlyContinue)) {
     Write-Host "已偵測到 Python：" (python --version)
 }
 
-pip3 install -r requirements.txt
 cd ~/Desktop && git clone https://github.com/CSIE-Camp/example-code-2025.git
+cd ~/Desktop/example-code-2025 && pip3 install -r requirements.txt
